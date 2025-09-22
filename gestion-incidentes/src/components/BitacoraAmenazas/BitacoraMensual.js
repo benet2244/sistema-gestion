ec4e2c89d1f60b4f9e33219610fecb896ade68ea
benetcolindres <benetcolindres@gmail.com>
2025-09-22 22:00:54 +0000

    actualizacion

--- gestion-incidentes/src/components/BitacoraAmenazas/BitacoraMensual.js
+++ gestion-incidentes/src/components/BitacoraAmenazas/BitacoraMensual.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './bitacoraAmenazas.css';
 
 const BitacoraMensual = () => {
     const [mes, setMes] = useState('');
